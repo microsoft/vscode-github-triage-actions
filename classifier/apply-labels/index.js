8c5b2c4776c8f1e6334bbb55d4461c1da00f1abb
Jackson Kearl <jakearl@microsoft.com>
2020-04-27 10:32:18 -0700

    Add ability to configure how classifier acts

--- classifier/apply-labels/index.js
+++ classifier/apply-labels/index.js
@@ -7,10 +7,28 @@ const github_1 = require("@actions/github");
 const octokit_1 = require("../../api/octokit");
 const utils_1 = require("../../utils/utils");
 const token = utils_1.getRequiredInput('token');
+const allowLabels = utils_1.getRequiredInput('allowLabels').split('|');
 const main = async () => {
-    const _github = new octokit_1.OctoKit(token, github_1.context.repo);
-    const file = JSON.parse(fs_1.readFileSync(path_1.join(__dirname, '../issue_labels.json'), { encoding: 'utf8' }));
-    console.log('Got labelings', JSON.stringify(file, null, 2));
+    const github = new octokit_1.OctoKit(token, github_1.context.repo);
+    const config = await github.readConfig(utils_1.getRequiredInput('config-path'));
+    const labelings = JSON.parse(fs_1.readFileSync(path_1.join(__dirname, '../issue_labels.json'), { encoding: 'utf8' }));
+    for (const labeling of labelings) {
+        const label = labeling.labels.length === 1 ? labeling.labels[0] : undefined;
+        const issue = new octokit_1.OctoKitIssue(token, github_1.context.repo, { number: labeling.number });
+        const issueData = await issue.getIssue();
+        if (!label ||
+            issueData.assignee ||
+            issueData.numComments ||
+            issueData.labels.some((label) => !allowLabels.includes(label))) {
+            continue;
+        }
+        const labelConfig = config[label];
+        await Promise.all([
+            (labelConfig === null || labelConfig === void 0 ? void 0 : labelConfig.skipLabeling) ? Promise.resolve() : issue.addLabel(label),
+            (labelConfig === null || labelConfig === void 0 ? void 0 : labelConfig.comment) ? issue.postComment(labelConfig.comment) : Promise.resolve(),
+            ...((labelConfig === null || labelConfig === void 0 ? void 0 : labelConfig.assign) ? labelConfig.assign.map((assignee) => issue.addAssignee(assignee)) : []),
+        ]);
+    }
 };
 main()
     .then(() => utils_1.logRateLimit(token))
--- classifier/apply-labels/index.ts
+++ classifier/apply-labels/index.ts
@@ -2,15 +2,42 @@ import * as core from '@actions/core'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import { context } from '@actions/github'
-import { OctoKit } from '../../api/octokit'
+import { OctoKit, OctoKitIssue } from '../../api/octokit'
 import { getRequiredInput, logErrorToIssue, logRateLimit } from '../../utils/utils'
 
 const token = getRequiredInput('token')
+const allowLabels = getRequiredInput('allowLabels').split('|')
+
+type ClassifierConfig = {
+	[area: string]: { skipLabeling?: boolean; comment?: string; assign?: [string] }
+}
 
 const main = async () => {
-	const _github = new OctoKit(token, context.repo)
-	const file = JSON.parse(readFileSync(join(__dirname, '../issue_labels.json'), { encoding: 'utf8' }))
-	console.log('Got labelings', JSON.stringify(file, null, 2))
+	const github = new OctoKit(token, context.repo)
+	const config: ClassifierConfig = await github.readConfig(getRequiredInput('config-path'))
+	const labelings: { number: number; labels: string[] }[] = JSON.parse(
+		readFileSync(join(__dirname, '../issue_labels.json'), { encoding: 'utf8' }),
+	)
+	for (const labeling of labelings) {
+		const label = labeling.labels.length === 1 ? labeling.labels[0] : undefined
+		const issue = new OctoKitIssue(token, context.repo, { number: labeling.number })
+		const issueData = await issue.getIssue()
+		if (
+			!label ||
+			issueData.assignee ||
+			issueData.numComments ||
+			issueData.labels.some((label) => !allowLabels.includes(label))
+		) {
+			continue
+		}
+
+		const labelConfig = config[label]
+		await Promise.all<any>([
+			labelConfig?.skipLabeling ? Promise.resolve() : issue.addLabel(label),
+			labelConfig?.comment ? issue.postComment(labelConfig.comment) : Promise.resolve(),
+			...(labelConfig?.assign ? labelConfig.assign.map((assignee) => issue.addAssignee(assignee)) : []),
+		])
+	}
 }
 
 main()
