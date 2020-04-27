0e45b7bf4d734c22e24e55bca85494e0b637d8a9
Jackson Kearl <jakearl@microsoft.com>
2020-04-27 07:25:55 -0700

    i hate python so so much

--- classifier/generate-labels/main.py
+++ classifier/generate-labels/main.py
@@ -73,6 +73,6 @@ if __name__ == "__main__":
 
     print("Generated labels: ")
     for issue in results:
-        print(issue["number"] + ": " + issue["labels"])
+        print(str(issue["number"]) + ": " + issue["labels"])
     with open(os.path.join(base_path, "issue_labels.json"), "w") as f:
         json.dump(results, f)
