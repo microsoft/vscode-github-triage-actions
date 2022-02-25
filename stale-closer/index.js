95d1af7436ed8e15d60184fcbc2df7972faa1c3b
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-02-25 15:04:08 -0500

    Update JS files

--- stale-closer/index.js
+++ stale-closer/index.js
@@ -5,34 +5,32 @@
  *--------------------------------------------------------------------------------------------*/
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("../common/utils");
-const FeatureRequest_1 = require("./FeatureRequest");
+const FeatureRequest_1 = require("../feature-request/FeatureRequest");
 const Action_1 = require("../common/Action");
 const config = {
     milestones: {
         candidateID: +utils_1.getRequiredInput('candidateMilestoneID'),
         candidateName: utils_1.getRequiredInput('candidateMilestoneName'),
-        backlogID: +utils_1.getRequiredInput('backlogMilestoneID'),
     },
     featureRequestLabel: utils_1.getRequiredInput('featureRequestLabel'),
     upvotesRequired: +utils_1.getRequiredInput('upvotesRequired'),
     numCommentsOverride: +utils_1.getRequiredInput('numCommentsOverride'),
     labelsToExclude: (utils_1.getInput('labelsToExclude') || '').split(',').filter((l) => !!l),
     comments: {
-        init: utils_1.getRequiredInput('initComment'),
+        init: utils_1.getInput('initComment'),
         warn: utils_1.getRequiredInput('warnComment'),
-        accept: utils_1.getRequiredInput('acceptComment'),
         reject: utils_1.getRequiredInput('rejectComment'),
-        rejectLabel: utils_1.getInput('rejectLabel')
+        rejectLabel: utils_1.getInput('rejectLabel'),
     },
     delays: {
         warn: +utils_1.getRequiredInput('warnDays'),
         close: +utils_1.getRequiredInput('closeDays'),
     },
 };
-class FeatureRequest extends Action_1.Action {
+class StaleCloser extends Action_1.Action {
     constructor() {
         super(...arguments);
-        this.id = 'FeatureRequest';
+        this.id = 'StaleCloser';
     }
     async onTriggered(github) {
         await new FeatureRequest_1.FeatureRequestQueryer(github, config).run();
@@ -42,9 +40,6 @@ class FeatureRequest extends Action_1.Action {
             await new FeatureRequest_1.FeatureRequestOnLabel(github, +utils_1.getRequiredInput('milestoneDelaySeconds'), config.milestones.candidateID, config.featureRequestLabel).run();
         }
     }
-    async onMilestoned(github) {
-        await new FeatureRequest_1.FeatureRequestOnMilestone(github, config.comments.init, config.milestones.candidateID).run();
-    }
 }
-new FeatureRequest().run(); // eslint-disable-line
+new StaleCloser().run(); // eslint-disable-line
 //# sourceMappingURL=index.js.map
