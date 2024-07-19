c98912776a355c4dd125596e60f99f951bcfbc00
Bhavya U <bhavyau@microsoft.com>
2024-07-19 13:56:12 -0700

    Update classifier-trainer inputs and authentication

--- classifier/train/fetch-issues/index.ts
+++ classifier/train/fetch-issues/index.ts
@@ -3,17 +3,16 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { context } from '@actions/github';
-import { getRequiredInput, getInput } from '../../../common/utils';
-import { download } from './download';
-import { createDataDirectories } from './createDataDir';
 import { statSync } from 'fs';
 import { join } from 'path';
 import { Action } from '../../../common/Action';
+import { getInput, getRequiredInput } from '../../../common/utils';
+import { createDataDirectories } from './createDataDir';
+import { download } from './download';
 
-const token = getRequiredInput('token');
 const endCursor = getInput('cursor');
-
+const owner = getRequiredInput('owner');
+const repo = getRequiredInput('repo');
 const areas = getRequiredInput('areas').split('|');
 const assignees = getRequiredInput('assignees').split('|');
 
@@ -22,12 +21,12 @@ class FetchIssues extends Action {
 
 	async onTriggered() {
 		if (endCursor) {
-			await download(token, context.repo, endCursor);
+			await download({ owner, repo }, endCursor);
 		} else {
 			try {
 				statSync(join(__dirname, 'issues.json')).isFile();
 			} catch {
-				await download(token, context.repo);
+				await download({ owner, repo });
 			}
 		}
 		await new Promise((resolve) => setTimeout(resolve, 1000));
