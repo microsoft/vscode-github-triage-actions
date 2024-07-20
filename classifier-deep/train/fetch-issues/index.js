d49f14ded0dc7b76c1cc4d0e17e525268df26aa2
Bhavya U <bhavyau@microsoft.com>
2024-07-19 17:20:58 -0700

    Update classifier trainer inputs

--- classifier-deep/train/fetch-issues/index.js
+++ classifier-deep/train/fetch-issues/index.js
@@ -4,7 +4,6 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 Object.defineProperty(exports, "__esModule", { value: true });
-const github_1 = require("@actions/github");
 const child_process_1 = require("child_process");
 const fs_1 = require("fs");
 const path_1 = require("path");
@@ -12,8 +11,9 @@ const Action_1 = require("../../../common/Action");
 const utils_1 = require("../../../common/utils");
 const blobStorage_1 = require("../../blobStorage");
 const download_1 = require("./download");
-const token = (0, utils_1.getRequiredInput)('token');
 const endCursor = (0, utils_1.getInput)('cursor');
+const owner = (0, utils_1.getRequiredInput)('owner');
+const repo = (0, utils_1.getRequiredInput)('repo');
 const blobContainer = (0, utils_1.getRequiredInput)('blobContainerName');
 class FetchIssues extends Action_1.Action {
     constructor() {
@@ -22,14 +22,14 @@ class FetchIssues extends Action_1.Action {
     }
     async onTriggered() {
         if (endCursor) {
-            await (0, download_1.download)(token, github_1.context.repo, endCursor);
+            await (0, download_1.download)({ owner, repo }, endCursor);
         }
         else {
             try {
                 (0, fs_1.statSync)((0, path_1.join)(__dirname, 'issues.json')).isFile();
             }
             catch {
-                await (0, download_1.download)(token, github_1.context.repo);
+                await (0, download_1.download)({ owner, repo });
             }
         }
         await new Promise((resolve) => setTimeout(resolve, 1000));
