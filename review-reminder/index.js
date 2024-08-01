8b0306ef57daa56c93415dbd05b73d5c2d2b08ab
Bhavya U <bhavyau@microsoft.com>
2024-07-31 22:01:50 -0700

    Update review-reminder action.yml and index files (#303)

--- review-reminder/index.js
+++ review-reminder/index.js
@@ -4,18 +4,18 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("../common/utils");
-const Action_1 = require("../common/Action");
-const ReviewReminder_1 = require("./ReviewReminder");
 const vscodeTools_1 = require("../api/vscodeTools");
+const Action_1 = require("../common/Action");
+const utils_1 = require("../common/utils");
+const ReviewReminder_1 = require("./ReviewReminder");
 const slackToken = (0, utils_1.getRequiredInput)('slack_token');
-const auth = (0, utils_1.getRequiredInput)('token');
 class ReviewReminderAction extends Action_1.Action {
     constructor() {
         super(...arguments);
         this.id = 'ReviewReminder';
     }
     async onTriggered() {
+        const auth = await this.getToken();
         await new ReviewReminder_1.ReviewReminder(auth, slackToken, new vscodeTools_1.VSCodeToolsAPIManager()).run();
     }
 }
