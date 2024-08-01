4254f57cbb51e0f3e453a25c3a2b8ed74c232a46
Bhavya U <bhavyau@microsoft.com>
2024-07-31 22:39:14 -0700

    Log botName and no-op

--- classifier-deep/monitor/index.js
+++ classifier-deep/monitor/index.js
@@ -12,10 +12,12 @@ class DeepClassifierMonitor extends Action_1.Action {
         this.id = 'Classifier-Deep/Monitor';
     }
     async onAssigned(issue, assignee) {
+        (0, utils_1.safeLog)(`Assigned to ${assignee}`);
         const assigner = await issue.getAssigner(assignee);
+        (0, utils_1.safeLog)(`Assigner is  + ${assigner}`);
         if (assigner !== (0, utils_1.getRequiredInput)('botName')) {
-            await issue.removeLabel('triage-needed');
-            await issue.removeLabel('stale');
+            // await issue.removeLabel('triage-needed');
+            // await issue.removeLabel('stale');
         }
     }
     async onUnassigned(_issue, _assignee) {
