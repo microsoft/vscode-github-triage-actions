1d4ad3a5219f12809700423d022510a5ef788c02
Bhavya U <bhavyau@microsoft.com>
2025-02-14 13:02:56 -0800

    Update triage to also handle triaging stale issues and letting team members know that they can help with triaging. (#345)
    * Refactor triage action inputs to make them optional and implement stale issue triaging logic
    
    * Add optional working areas link input and update triage logic for team members
    
    * Update comment for stale issue filtering logic to clarify criteria

--- triage/index.js
+++ triage/index.js
@@ -4,6 +4,8 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const octokit_1 = require("../api/octokit");
 const vscodeTools_1 = require("../api/vscodeTools");
 const Action_1 = require("../common/Action");
 const utils_1 = require("../common/utils");
@@ -12,14 +14,23 @@ class IssueTriageAction extends Action_1.Action {
         super(...arguments);
         this.id = 'IssueTriageAction';
     }
-    async triage(issue) {
+    async triage(issue, skipTeamCheck = false) {
         try {
             const githubIssue = await issue.getIssue();
             if (!githubIssue)
                 return;
             const vscodeToolsAPI = new vscodeTools_1.VSCodeToolsAPIManager();
             const teamMembers = new Set((await vscodeToolsAPI.getTeamMembers()).map((t) => t.id));
-            if (teamMembers.has(githubIssue.author.name)) {
+            if (!skipTeamCheck && teamMembers.has(githubIssue.author.name)) {
+                if (githubIssue.assignees.length === 0) {
+                    const link = (0, core_1.getInput)('workingAreasLink');
+                    if (link) {
+                        await issue.postComment(`Hi ${githubIssue.author.name}. As a member of the team, you can help us triage this issue by referring to ${link}`);
+                    }
+                    else {
+                        await issue.postComment(`Hi ${githubIssue.author.name}. You can help us triage this issue by assigning it to the appropriate person.`);
+                    }
+                }
                 (0, utils_1.safeLog)('Author is a team member, skipping triaging', githubIssue.author.name);
                 return;
             }
@@ -58,9 +69,28 @@ class IssueTriageAction extends Action_1.Action {
         }
     }
     async onOpened(issue) {
-        // wait 30 seconds before triaging
-        await new Promise((resolve) => setTimeout(resolve, 30000));
+        // wait 1 minute before triaging
+        await new Promise((resolve) => setTimeout(resolve, 60000));
         await this.triage(issue);
+    }
+    async onTriggered(_octokit) {
+        const owner = (0, utils_1.getRequiredInput)('owner');
+        const repo = (0, utils_1.getRequiredInput)('repo');
+        const token = await (0, Action_1.getAuthenticationToken)();
+        const staleIssues = _octokit.query({
+            q: `is:issue is:open no:assignee no:label updated:<${(0, utils_1.daysAgoToHumanReadbleDate)(7)}`,
+        });
+        // Loop through issues which are not assigned and no labels and updated more than 7 days ago
+        for await (const page of staleIssues) {
+            for (const issueData of page) {
+                const issue = await issueData.getIssue();
+                if (!issue)
+                    continue;
+                const octokitIssue = new octokit_1.OctoKitIssue(token, { owner, repo }, { number: issue === null || issue === void 0 ? void 0 : issue.number });
+                await this.triage(octokitIssue, true);
+            }
+        }
+        (0, utils_1.safeLog)('Completed triaging stale issues.');
     }
 }
 new IssueTriageAction().run(); // eslint-disable-line
--- triage/index.ts
+++ triage/index.ts
@@ -3,22 +3,35 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { OctoKitIssue } from '../api/octokit';
+import { getInput } from '@actions/core';
+import { OctoKit, OctoKitIssue } from '../api/octokit';
 import { VSCodeToolsAPIManager } from '../api/vscodeTools';
-import { Action } from '../common/Action';
-import { getRequiredInput, safeLog } from '../common/utils';
+import { Action, getAuthenticationToken } from '../common/Action';
+import { daysAgoToHumanReadbleDate, getRequiredInput, safeLog } from '../common/utils';
 
 class IssueTriageAction extends Action {
 	id = 'IssueTriageAction';
 
-	private async triage(issue: OctoKitIssue) {
+	private async triage(issue: OctoKitIssue, skipTeamCheck = false): Promise<void> {
 		try {
 			const githubIssue = await issue.getIssue();
 			if (!githubIssue) return;
 
 			const vscodeToolsAPI = new VSCodeToolsAPIManager();
 			const teamMembers = new Set((await vscodeToolsAPI.getTeamMembers()).map((t) => t.id));
-			if (teamMembers.has(githubIssue.author.name)) {
+			if (!skipTeamCheck && teamMembers.has(githubIssue.author.name)) {
+				if (githubIssue.assignees.length === 0) {
+					const link = getInput('workingAreasLink');
+					if (link) {
+						await issue.postComment(
+							`Hi ${githubIssue.author.name}. As a member of the team, you can help us triage this issue by referring to ${link}`,
+						);
+					} else {
+						await issue.postComment(
+							`Hi ${githubIssue.author.name}. You can help us triage this issue by assigning it to the appropriate person.`,
+						);
+					}
+				}
 				safeLog('Author is a team member, skipping triaging', githubIssue.author.name);
 				return;
 			}
@@ -62,9 +75,30 @@ class IssueTriageAction extends Action {
 	}
 
 	protected override async onOpened(issue: OctoKitIssue): Promise<void> {
-		// wait 30 seconds before triaging
-		await new Promise((resolve) => setTimeout(resolve, 30000));
+		// wait 1 minute before triaging
+		await new Promise((resolve) => setTimeout(resolve, 60000));
 		await this.triage(issue);
+	}
+
+	protected override async onTriggered(_octokit: OctoKit): Promise<void> {
+		const owner = getRequiredInput('owner');
+		const repo = getRequiredInput('repo');
+		const token = await getAuthenticationToken();
+
+		const staleIssues = _octokit.query({
+			q: `is:issue is:open no:assignee no:label updated:<${daysAgoToHumanReadbleDate(7)}`,
+		});
+
+		// Loop through issues which are not assigned and no labels and updated more than 7 days ago
+		for await (const page of staleIssues) {
+			for (const issueData of page) {
+				const issue = await issueData.getIssue();
+				if (!issue) continue;
+				const octokitIssue = new OctoKitIssue(token, { owner, repo }, { number: issue?.number });
+				await this.triage(octokitIssue, true);
+			}
+		}
+		safeLog('Completed triaging stale issues.');
 	}
 }
 
